80db91e4cc0635c2bb90a9f0736b62c2e9836e19
TanKhuu <jackiekhuu.work@gmail.com>
2017-04-05 18:16:35 +0700

    sync loans, saving, dpd, due installments, icaremembers

--- imports/api/olap/migrations/customers.js
+++ imports/api/olap/migrations/customers.js
@@ -9,6 +9,31 @@ import scripts from './scripts';
 const {batches} = Meteor.settings.elastic.migration;
 
 /**
+ * Function get run time in seconds
+ * @param start
+ * @return {String} run time in seconds
+ */
+const getRunTime = (start) => {
+  const end = new Date();
+  return accounting
+    .formatNumber(Number(moment(end).diff(moment(start), 'seconds')));
+};
+
+/**
+ *
+ * @param data
+ * @param unusedFields
+ * @return {*}
+ */
+const removeUnusedFields = ({data, unusedFields}) => {
+  const fields = data;
+  unusedFields.map(f => {
+    delete fields[f];
+  });
+  return fields;
+};
+
+/**
  * Function sync customers basic information
  * @param source
  * @param dest
@@ -38,9 +63,42 @@ const syncCustomersBasicInfo = ({source, dest, script, options}) => {
   }
 };
 
-const syncBusinessUnitsBasicInfo = ({source, dest}) => {
-  const action = 'syncCustomersBasicInfo';
-  /* get all _id of business units */
+/**
+ * Function sync business units basic information
+ * @param source
+ * @param dest
+ * @param script
+ * @param options
+ */
+const syncBusinessUnitsBasicInfo = ({source, dest, script, options}) => {
+  const action = 'syncBusinessUnitsBasicInfo';
+  try {
+    // docs: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-reindex
+    const body = {source, dest};
+    !_.isEmpty(script) && (body.script = script);
+    const result = Elastic.reindex({
+      ...options,
+      body
+    });
+
+    /* Success */
+    return {result: {name: action, message: JSON.stringify(result)}};
+
+  } catch (e) {
+    /* Failed */
+    return {error: {name: action, message: JSON.stringify(e)}};
+  }
+};
+
+/**
+ * Function sync business units into customers
+ * @param source
+ * @param dest
+ * @return {*}
+ */
+const syncBusinessUnitsIntoCustomers = ({source, dest}) => {
+  const action = 'syncBusinessUnitsBasicInfoIntoCustomers';
+  /* get all _id of customers */
   const
     {index, type} = dest,
     body = {
@@ -51,7 +109,7 @@ const syncBusinessUnitsBasicInfo = ({source, dest}) => {
 
   try {
     // get total customers
-    const {hits: {total, hits}} = Elastic.search({
+    const {hits: {total}} = Elastic.search({
       index,
       type,
       body,
@@ -100,14 +158,9 @@ const syncBusinessUnitsBasicInfo = ({source, dest}) => {
 
                   const BUs = hits.map(({_source}) => {
                     /* remove unused fields */
-                    delete _source.country;
-                    delete _source.type;
-                    delete _source["@timestamp"];
-                    delete _source["@version"];
-                    delete _source.type;
-                    delete _source.type;
-
-                    return _source;
+                    const unusedFields = ['country', 'type', '@timestamp', '@version',];
+                    const fields = removeUnusedFields({data: _source, unusedFields});
+                    return fields;
                   });
 
                   try {
@@ -123,24 +176,139 @@ const syncBusinessUnitsBasicInfo = ({source, dest}) => {
                       }
                     });
 
+                    Logger.info({name: action, message: JSON.stringify(addBUs)});
+
                   } catch (e) {
                     /* Failed */
-                    Logger.warn({name: 'syncBusinessUnitsBasicInfo', message: JSON.stringify(e)});
+                    Logger.warn({name: action, message: JSON.stringify(e)});
                   }
                 }
               } else {
                 Logger.warn({
-                  name: 'syncBusinessUnitsBasicInfo',
+                  name: action,
                   message: JSON.stringify({index, type, body})
                 });
               }
             } catch (e) {
-              Logger.warn({name: 'syncBusinessUnitsBasicInfo', message: JSON.stringify(e)});
+              Logger.warn({name: action, message: JSON.stringify(e)});
             }
           });
         } catch (e) {
           /* Failed */
-          Logger.warn({name: 'syncBusinessUnitsBasicInfo', message: JSON.stringify(e)});
+          Logger.warn({name: action, message: JSON.stringify(e)});
+        }
+      }
+
+      // Finish sync
+      return {result: {name: action, message: JSON.stringify({total})}};
+
+    } else {
+      return {result: {name: action, message: JSON.stringify({total})}};
+    }
+  } catch (e) {
+    /* Failed */
+    return {error: {name: action, message: JSON.stringify(e)}};
+  }
+};
+
+/**
+ * Function sync iCare Members basic information into icare_members
+ * @param source
+ * @param dest
+ * @param script
+ * @param options
+ * @return {*}
+ */
+const syncIcareMembersBasicInfo = ({source, dest, script, options}) => {
+  const action = 'syncIcareMembersBasicInfo';
+
+  try {
+    // docs: https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-reindex
+    const result = Elastic.reindex({
+      ...options,
+      body: {
+        source,
+        dest,
+        script
+      }
+    });
+
+    /* Success */
+    return {result: {name: action, message: JSON.stringify(result)}};
+
+  } catch (e) {
+    /* Failed */
+    return {error: {name: action, message: JSON.stringify(e)}};
+  }
+};
+
+/**
+ * Function sync iCare Members mifos information into icare_members
+ * @param source
+ * @param dest
+ * @return {*}
+ */
+const syncIcareMembersMifosInfo = ({source, dest}) => {
+  const action = 'syncIcareMembersMifosInfo';
+  /* get all _id of magento_customer_id */
+  const
+    {index, type} = source,
+    body = {
+      query: {
+        match_all: {}
+      }
+    };
+
+  try {
+    // get total mifos clients
+    const {hits: {total, hits}} = Elastic.search({
+      index,
+      type,
+      body,
+      size: 0,
+    });
+
+    if (total > 0) {
+      /* get mifos clients to import into icare_members */
+      for (let i = 0; i < total; i += batches) {
+        body.from = i;
+        body.size = batches;
+
+        try {
+          const {hits: {total, hits: clients}} = Elastic.search({
+            index,
+            type,
+            body,
+          });
+
+          clients.map(c => {
+            const {_source: {clientExternalId, loans, dueInstallments, dpdInfo, totalSavingBalance}} = c;
+
+            if (clientExternalId) {
+              const {index, type} = dest;
+              const doc = {};
+
+              !_.isEmpty(loans) && (doc.loans = loans)
+              !_.isEmpty(dpdInfo) && (doc.dpd = dpdInfo);
+              !_.isEmpty(totalSavingBalance) && (doc.saving = totalSavingBalance);
+              !_.isEmpty(dueInstallments) && (doc.due_installments = dueInstallments);
+
+              const addMifosInfo = Elastic.update({
+                index,
+                type,
+                id: clientExternalId,
+                body: {doc}
+              });
+
+              Logger.info({name: action, message: {index, type, addMifosInfo}});
+
+            } else {
+              Logger.warn({name: action, message: {index, type, client: c}})
+            }
+          });
+        } catch (e) {
+          /* Failed */
+          Logger.warn({name: action, message: JSON.stringify(e)});
         }
       }
 
@@ -149,6 +317,132 @@ const syncBusinessUnitsBasicInfo = ({source, dest}) => {
 
     } else {
       return {result: {name: action, message: JSON.stringify({total, hits})}};
+    }
+  } catch (e) {
+    /* Failed */
+    return {error: {name: action, message: JSON.stringify(e)}};
+  }
+};
+
+/**
+ * Function sync iCare Members into Business Units
+ * @param source
+ * @param dest
+ * @return {*}
+ */
+const syncIcareMembersIntoBusinessUnits = ({source, dest}) => {
+  const action = 'syncIcareMembersIntoBusinessUnits';
+
+  /* get total business units */
+  const
+    {index, type} = dest,
+    body = {
+      query: {
+        match_all: {}
+      }
+    };
+
+  try {
+    // get total business units
+    const {hits: {total}} = Elastic.search({
+      index,
+      type,
+      body,
+      size: 0,
+    });
+
+    console.log('total business units', total);
+
+    if (total > 0) {
+      /* get icare members for all business units */
+      for (let i = 0; i < total; i += batches) {
+        body.from = i;
+        body.size = batches;
+
+        try {
+          const {hits: {hits: BUs}} = Elastic.search({
+            index,
+            type,
+            _source: false,
+            body,
+          });
+
+          console.log('BUs', BUs);
+
+          BUs.map(bu => {
+            const {_id} = bu;
+            const
+              {index, type} = source,
+              body = bodybuilder()
+                .query('term', 'netsuite_business_unit_id', _id)
+                .build()
+              ;
+            try {
+              body.size = 0;
+              const {hits: {total}} = Elastic.search({
+                index,
+                type,
+                body,
+              });
+              console.log('bu _id', _id, 'total icms', total)
+              if (total > 0) {
+                for (let i = 0; i < total; i += batches) {
+                  body.from = i;
+                  body.size = batches;
+
+                  const {hits: {hits}} = Elastic.search({
+                    index,
+                    type,
+                    body,
+                  });
+
+                  console.log('icms', hits);
+
+                  const iCMs = hits.map(({_source}) => {
+                    return _source;
+                  });
+
+                  try {
+                    const {index, type} = dest;
+                    const addiCMs = Elastic.update({
+                      index,
+                      type,
+                      id: _id,
+                      body: {
+                        doc: {
+                          icare_members: iCMs
+                        }
+                      }
+                    });
+
+                    Logger.info({name: action, message: JSON.stringify(addiCMs)});
+
+                  } catch (e) {
+                    /* Failed */
+                    Logger.warn({name: action, message: JSON.stringify(e)});
+                  }
+                }
+              } else {
+                Logger.warn({
+                  name: action,
+                  message: JSON.stringify({index, type, body})
+                });
+              }
+            } catch (e) {
+              Logger.warn({name: action, message: JSON.stringify(e)});
+            }
+          });
+        } catch (e) {
+          /* Failed */
+          Logger.warn({name: action, message: JSON.stringify(e)});
+        }
+      }
+
+      // Finish sync
+      return {result: {name: action, message: JSON.stringify({total})}};
+
+    } else {
+      return {result: {name: action, message: JSON.stringify({total})}};
     }
   } catch (e) {
     /* Failed */
@@ -169,6 +463,9 @@ const customers = ({country}) => {
       public: {env} // running environment
     } = Meteor.settings,
     suffix = moment(runDate).format('YYYY.MM.DD-HH.mm'), // elastic index suffix
+    newIndex = `${prefix}_${country}_${env}-${suffix}`,
+    baseIndex = `icare_${env}_${country}`,
+    tempIndex = `${prefix}_${country}_${env}_tmp`,
     alias = `${prefix}_${country}_${env}`
     ;
 
@@ -176,52 +473,201 @@ const customers = ({country}) => {
     migrate: () => {
       const
         source = {
-          index: `icare_${env}_${country}`,
+          index: baseIndex,
           type: 'b2b_customer',
         },
         dest = {
-          index: `${prefix}_${country}_${env}-${suffix}`,
+          index: newIndex,
           type: 'customers',
         },
-        {lang, bots: {customers: {netsuiteId, customerId, name}}} = scripts,
-        script = {
-          lang,
-          inline: `${netsuiteId};${customerId};${name}`
-        },
+        {lang, bots: {customers: customersScripts}} = scripts,
         options = {refresh: true, waitForCompletion: true};
+      const script = {
+        lang,
+        inline: Object.keys(customersScripts)
+          .map(s => customersScripts[s])
+          .join(';')
+      };
 
-      console.log('syncCustomersBasicInfo', {runDate, source, dest, script, options});
       /* sync customers basic information */
+      // console.log('syncCustomersBasicInfo', {source, dest, script, options});
       const {error, result} = syncCustomersBasicInfo({source, dest, script, options});
       if (error) {
         /* failed */
-        Logger.error(error);
+        Logger.error({
+          name: 'MIGRATE_CUSTOMERS.syncCustomersBasicInfo',
+          message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+        });
+        return {
+          error: {
+            name: 'MIGRATE_CUSTOMERS.syncCustomersBasicInfo',
+            message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+          }
+        };
       } else {
         /* success */
-        Logger.info(result);
+        Logger.info({
+          name: 'syncCustomersBasicInfo',
+          message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+        });
 
         /* sync business units basic information */
         const
           source = {
-            index: `${prefix}_${country}_${env}_tmp`,
+            index: tempIndex,
             type: 'business_units',
           },
           dest = {
-            index: `${prefix}_${country}_${env}-${suffix}`,
-            type: 'customers',
-          };
-        const {error, result} = syncBusinessUnitsBasicInfo({source, dest});
+            index: newIndex,
+            type: 'business_units',
+          },
+          script = {};
+        const {error, result} = syncBusinessUnitsBasicInfo({source, dest, script, options});
         if (error) {
-          return {error: {name: 'MIGRATE_CUSTOMERS', message: error}}
+          /* failed */
+          Logger.error({
+            name: 'MIGRATE_CUSTOMERS.syncBusinessUnitsBasicInfo',
+            message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+          });
+          return {
+            error: {
+              name: 'MIGRATE_CUSTOMERS.syncBusinessUnitsBasicInfo',
+              message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+            }
+          };
         } else {
-          const finishDate = new Date();
-          const runTime = accounting
-            .formatNumber(Number(moment(finishDate).diff(moment(runDate), 'seconds')));
+          /* success */
+          Logger.info({
+            name: 'syncBusinessUnitsBasicInfo',
+            message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+          });
 
-          return {result: {
-            name: 'MIGRATE_CUSTOMERS',
-            message: {runTime: `${runTime} seconds`, result}}};
+          /* sync icare members basic information */
+          const
+            source = {
+              index: baseIndex,
+              type: 'customer',
+            },
+            dest = {
+              index: newIndex,
+              type: 'icare_members',
+            },
+            {lang, bots: {icareMembers: iCMsScripts}} = scripts;
+          const script = {
+            lang,
+            inline: Object.keys(iCMsScripts)
+              .map(s => iCMsScripts[s])
+              .join(';')
+          };
+
+          const {error, result} = syncIcareMembersBasicInfo({source, dest, script, options});
+          if (error) {
+            /* failed */
+            Logger.error({
+              name: 'MIGRATE_CUSTOMERS.syncIcareMembersBasicInfo',
+              message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+            });
+            return {
+              error: {
+                name: 'MIGRATE_CUSTOMERS.syncIcareMembersBasicInfo',
+                message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+              }
+            };
+          } else {
+            /* success */
+            Logger.info({
+              name: 'syncIcareMembersBasicInfo',
+              message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+            });
+
+            /* sync iCare Members loans, saving, dpd, due_installments */
+            const
+              source = {
+                index: baseIndex,
+                type: 'm_client',
+              },
+              dest = {
+                index: newIndex,
+                type: 'icare_members',
+              };
+            // const {error, result} = syncIcareMembersMifosInfo({source, dest});
+            const error = null, result = {name: 'syncIcareMembersMifosInfo', message: 'skipped'};
+            if (error) {
+              /* failed */
+              Logger.error({
+                name: 'MIGRATE_CUSTOMERS.syncIcareMembersMifosInfo',
+                message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+              });
+              return {
+                error: {
+                  name: 'MIGRATE_CUSTOMERS.syncIcareMembersMifosInfo',
+                  message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+                }
+              };
+            } else {
+              /* success */
+              Logger.info({
+                name: 'syncIcareMembersMifosInfo',
+                message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+              });
+
+              /* sync icare members into business units */
+              const
+                source = {
+                  index: newIndex,
+                  type: 'icare_members',
+                },
+                dest = {
+                  index: newIndex,
+                  type: 'business_units',
+                };
+              const {error, result} = syncIcareMembersIntoBusinessUnits({source, dest});
+              if (error) {
+                /* failed */
+                Logger.error({
+                  name: 'MIGRATE_CUSTOMERS.syncIcareMembersIntoBusinessUnits',
+                  message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+                });
+                return {
+                  error: {
+                    name: 'MIGRATE_CUSTOMERS.syncIcareMembersIntoBusinessUnits',
+                    message: {runTime: `${getRunTime(runDate)} seconds`, error: JSON.stringify(error)}
+                  }
+                };
+              } else {
+                /* success */
+                Logger.info({
+                  name: 'syncIcareMembersIntoBusinessUnits',
+                  message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+                });
+
+                return {
+                  result: {
+                    name: 'MIGRATE_CUSTOMERS',
+                    message: {runTime: `${getRunTime(runDate)} seconds`, result: JSON.stringify(result)}
+                  }
+                };
+              }
+            }
+          }
         }
+
+
+
+        /* sync business units basic information */
+        // const
+        //   source = {
+        //     index: `${prefix}_${country}_${env}_tmp`,
+        //     type: 'business_units',
+        //   },
+        //   dest = {
+        //     index: `${prefix}_${country}_${env}-${suffix}`,
+        //     type: 'customers',
+        //   };
+        // const {error, result} = syncBusinessUnitsBasicInfo({source, dest});
+        // if (error) {
+        //   return {error: {name: 'MIGRATE_CUSTOMERS', message: error}}
+        // }
       }
     }
   }
--- imports/api/olap/migrations/scripts.js
+++ imports/api/olap/migrations/scripts.js
@@ -5,7 +5,30 @@ const scripts = {
       netsuiteId: "ctx._source.netsuite_id = ctx._source.remove(\"id\")",
       customerId: "ctx._source.customer_id = ctx._source.remove(\"b2b_customer_id\")",
       name: "ctx._source.name = ctx._source.remove(\"company_name\")",
-    }
+    },
+    icareMembers: {
+      name: "ctx._source.name = ctx._source.remove(\"full_name\")",
+      phone: "ctx._source.phone = ctx._source.remove(\"telephone\")",
+      magentoCustomerId: "ctx._source.magento_customer_id = ctx._source.remove(\"id\")",
+      mifosClientId: "ctx._source.mifos_client_id = ctx._source.remove(\"m_client_id\")",
+      netsuiteBusinessUnitId: "ctx._source.netsuite_business_unit_id = ctx._source.remove(\"business_unit_id\")",
+      netsuiteCustomerId: "ctx._source.netsuite_customer_id = ctx._source.remove(\"organization_id\")",
+      inactivate: "ctx._source.inactivate = !(ctx._source.remove(\"is_active\") != 0)",
+      availableCreditLimit: "ctx._source.available_credit_limit = ctx._source.remove(\"m_available_credit_limit\")",
+      availableDueLimit: "ctx._source.available_due_limit = ctx._source.remove(\"m_available_due_limit\")",
+      maternityLeaveStartDate: "ctx._source.maternity_leave_start_date = ctx._source.remove(\"maternity_start_date\")",
+      maternityLeaveEndDate: "ctx._source.maternity_leave_end_date = ctx._source.remove(\"maternity_end_date\")",
+      lastPurchaseDate: "ctx._source.last_purchase_date = ctx._source.remove(\"last_purchased\")",
+      defaultShippingAddress: "ctx._source.default_shipping_address = ctx._source.remove(\"defaultShippingAddress\")",
+      nextPayments: "ctx._source.next_payments = ctx._source.remove(\"m_next_payments\")",
+      totalLoanBalance: "ctx._source.total_loan_balance = ctx._source.remove(\"m_total_loan_balance\")",
+      totalLoanPrincipal: "ctx._source.total_loan_principal = ctx._source.remove(\"m_total_loan_principal\")",
+      totalPaidAmount: "ctx._source.total_paid_amount = ctx._source.remove(\"m_total_paid_amount\")",
+      totalIsingCreditAmount: "ctx._source.total_using_credit_amount = ctx._source.remove(\"m_total_using_credit_amount\")",
+      totalUsingDueAmount: "ctx._source.total_using_due_amount = ctx._source.remove(\"m_total_using_due_amount\")",
+      clientId: "ctx._source.remove(\"client_id\")",
+      mClient: "ctx._source.remove(\"m_client\")",
+    },
   }
 };
 
