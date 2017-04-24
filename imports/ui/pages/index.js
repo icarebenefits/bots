bc9efcc3cce3a065a004411ed3db9978f5336529
TanKhuu <jackiekhuu.work@gmail.com>
2017-04-24 10:00:54 +0700

    refactor Workplace page- simple UI

--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -4,7 +4,8 @@ export {default as BlankPage} from './Blank';
 // Main Pages
 export {default as HomePage} from './Home';
 export {default as CountriesPage} from './Countries';
-export {default as WorkplacesPage} from './Workplaces';
+export {default as WorkplacesPage} from './Workplaces.new';
+// export {default as WorkplacesPage} from './Workplaces';
 export {default as SLAsPage} from './SLAs';
 
 // Error Pages
