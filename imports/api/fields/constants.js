d2b80a6d0e622e4d4c4f05b47dcaadcd0ee7bbba
TanKhuu <jackiekhuu.work@gmail.com>
2017-05-15 23:48:26 +0700

    get fields need suggests and placeHolder

---
+++ imports/api/fields/constants.js
@@ -0,0 +1,48 @@
+export const PLACEHOLDER = {
+  name: '',
+  phone: '',
+  email: '',
+  address: '',
+  jobTitle: '',
+  department: '',
+  paymentMethod: '',
+  typeOfContract: '',
+  bankName: '',
+  status: '',
+  soNumber: '',
+  purchaseStore: '',
+  purchasedBy: '',
+  purchasedByChannel: '',
+  shipToName: '',
+  shipToAddress: '',
+  billToName: '',
+  billToAddress: '',
+  ticketId: '',
+  ticketTitle: '',
+  ticketType: '',
+  ticketPriority: '',
+  staffName: '',
+  staffEmail: '',
+};
+
+export const SUGGESTS = {
+  customer: {
+    paymentMethod: [],
+    typeOfContract: [],
+    bankName: []
+  },
+  iCareMember: {
+    paymentMethod: [],
+    typeOfContract: [],
+    bankName: []
+  },
+  salesOrder: {
+    status: [],
+    purchaseStore: [],
+    purchasedByChannel: []
+  },
+  ticket: {
+    type: [],
+    priority: []
+  }
+};
