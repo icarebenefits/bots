365e1a1e9609cc7fa6eadd09695f95add53b16dd
TanKhuu <jackiekhuu.work@gmail.com>
2017-06-01 12:54:21 +0700

    Refactor Publish SLA with Promise

---
+++ imports/api/collections/slas/functions.js
@@ -0,0 +1,65 @@
+import {Promise} from 'meteor/promise';
+import {DDP} from 'meteor/ddp-client';
+
+/* Collections */
+import SLAs from './slas';
+
+export const publishSLA = (_id, country) => {
+  let publishedUrl = '';
+  // get SLA info
+  return new Promise((resolve, reject) => {
+    const sla = SLAs.findOne({_id});
+    if (!_.isEmpty(sla)) {
+      try {
+        // connect to production with DDP protocol
+        const {host} = Meteor.settings.public.prod;
+        const BotsProd = DDP.connect(`http://${host}`);
+
+        const
+          {
+            name: originalName, description,
+            frequency, conditions, message
+          } = sla;
+        const
+          name = `${originalName}_published`,
+          workplace = '',
+          status = 'draft';
+
+        // validate name for published SLA
+        BotsProd.call('sla.validateName', {name, country}, (err, res) => {
+          if (err) {
+            reject(new Meteor.Error('VALIDATE_NAME_IN_PROD', err.reason));
+          } else {
+            const {validated, detail} = res;
+            if (!validated) {
+              reject(new Meteor.Error('VALIDATE_NAME_IN_PROD', detail[0]));
+            } else {
+              // publish SLA to production
+              BotsProd.call('sla.create', {
+                name, description, workplace,
+                frequency, conditions, message,
+                status, country
+              }, (err, res) => {
+                if (err)
+                  throw new Meteor.Error('CREATE_SLA_IN_PROD', err.reason);
+
+                const {_id} = res;
+                // return the published SLA URL
+                publishedUrl = `http://${host}/app/setup/${country}?tab=sla&mode=edit&id=${_id}`;
+
+                // disconnect from production
+                BotsProd.disconnect();
+
+                resolve({publishedUrl});
+              });
+            }
+          }
+        });
+      } catch (err) {
+        reject(new Meteor.Error('SLA.publish', err.message));
+      }
+    } else {
+      reject(new Meteor.Error('SLA.publish', 'SLA not found.'));
+    }
+  });
+};
