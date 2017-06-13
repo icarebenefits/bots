0bb072d0e47744307c58d62605352ddba1033092
TanKhuu <jackiekhuu.work@gmail.com>
2017-06-13 23:02:38 +0700

    index RFM index

--- imports/api/rfm/functions.js
+++ imports/api/rfm/functions.js
@@ -2,7 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {check, Match} from 'meteor/check';
 import moment from 'moment';
 import bodybuilder from 'bodybuilder';
-import _ from 'lodash';
+import {Promise} from 'meteor/promise';
 
 /* Elastic */
 import {ElasticClient as Elastic} from '/imports/api/elastic';
@@ -17,10 +17,10 @@ import {getIndexSuffix} from '/imports/utils';
  * @param {String} period - Ex: "now-1y"
  * @returns {recency, frequency, monetary}
  */
-export const calculateRFMScore = async({index, type, magento_customer_id, period, runDate}) => {
+export const calculateRFMScore = async({index, type, id, period, runDate}) => {
   check(index, String);
   check(type, String);
-  check(magento_customer_id, Match.OneOf(Number, String));
+  check(id, Match.OneOf(Number, String));
   check(period, String);
   check(runDate, Date);
 
@@ -33,7 +33,7 @@ export const calculateRFMScore = async({index, type, magento_customer_id, period
       MField = 'grand_total_purchase',
       body = bodybuilder()
         .size(0)
-        .filter('term', key, magento_customer_id)
+        .filter('term', key, id)
         .filter('range', periodField, {gte: period})
         .aggregation(aggType, RField)
         .aggregation(aggType, MField)
@@ -57,66 +57,83 @@ export const calculateRFMScore = async({index, type, magento_customer_id, period
   }
 };
 
+const indexRFMs = async({hits, source, dest, period, runDate, count}) => {
+  try {
+    let index = '', type = '';
+    await Promise.all(hits.map(
+      async hit => {
+        index = source.index;
+        type = source.type;
+        const {_id: id, _source} = hit;
+        const {recency, frequency, monetary} = await calculateRFMScore({index, type, id, period, runDate});
+        index = dest.index;
+        type = dest.type;
+        const body = {
+          ..._source,
+          recency,
+          frequency,
+          monetary
+        };
+        await Elastic.index({index, type, id, body});
+        count++;
+      }));
+    return {count};
+  } catch (err) {
+    throw new Meteor.Error('INDEX_RFMs', err.message);
+  }
+};
+
 export const indexRFMModel = async({country}) => {
   check(country, String);
 
   try {
     const
       runDate = new Date(),
+      {suffix} = getIndexSuffix(runDate, 'day'),
       {
         elastic: {indices: {bots: botsIndex, rfm: rfmIndex}},
         public: {env, elastic: {batches, scroll}}
       } = Meteor.settings,
+      period = rfmIndex.periods.year,
       source = {
         index: `${botsIndex.prefix}_${country}_${env}`,
         type: botsIndex.types.sales_order
       },
       dest = {
-        index: `${rfmIndex.prefix}_${country}_${env}`,
+        index: `${rfmIndex.prefix}_${country}_${env}-${suffix}`,
         type: rfmIndex.types.year
-      },
-      period = rfmIndex.periods.year,
-      {suffix} = getIndexSuffix(runDate, 'day');
+      };
     let index = '', type = '',
-      scrollId = '',
-      iCMList = []; // list of iCare member who have sales orders
+      scrollId = '';
 
     /* Index */
     // get all iCare members who have sales orders
     let body = bodybuilder()
       .query('has_child', {type: source.type}, (q) => {
         return q
+          .filter('range', 'purchase_date', {gte: period})
           .query('exists', 'field', 'magento_customer_id')
       })
       .build();
-    
+
     index = source.index;
     type = botsIndex.types.icare_member;
     body.size = batches;
-    body._source = false; // no need to receive the source document
+    body._source = ["name", "gender", "company", "business_unit", "email", "phone"];
 
-    const {hits: {hits, total}, _scroll_id} = await Elastic.search({index, type, scroll, body});
-    hits.forEach(hit => iCMList.push(hit._id));
-    scrollId = _scroll_id;
     let count = 0;
-    while(iCMList.length < total) {
-      console.log('iCMList', iCMList.length);
+    const {hits: {hits, total}, _scroll_id} = await Elastic.search({index, type, scroll, body});
+    const {count: runCount} = await indexRFMs({hits, source, dest, period, runDate, count});
+    count = runCount;
+    scrollId = _scroll_id;
+    while (count < total) {
       const {hits: {hits}, _scroll_id} = await Elastic.scroll({scrollId, scroll});
-      hits.forEach(hit => iCMList.push(hit._id));
+      const {count: runCount} = await indexRFMs({hits, source, dest, period, runDate, count});
+      count = runCount;
       scrollId = _scroll_id;
-      count++;
     }
 
-
-
-
-    // console.log('source', source);
-    // console.log('dest', dest);
-    // console.log('period', period);
-    // console.log('suffix', suffix);
-    // console.log('totalICMs', iCMList);
-
-    return {iCMList: iCMList.length};
+    return {count};
   } catch (err) {
     throw new Meteor.Error('INDEX_RFM_MODEL', err.message);
   }
